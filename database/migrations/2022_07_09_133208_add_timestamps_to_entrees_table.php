37954ba267e643b3c83d6aaf6d8d36b10e79d52d
Stan Rutten <stanrutten242@hotmail.com>
2022-07-09 16:12:19 +0200

    migrations

---
+++ database/migrations/2022_07_09_133208_add_timestamps_to_entrees_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('entrees', function (Blueprint $table) {
+            // before it was: 0000-00-00 00:00:00
+            $table->string('entree_enddate')->default(null)->change();
+            
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('entrees', function (Blueprint $table) {
+            $table->dropColumn('created_at');
+            $table->dropColumn('updated_at');
+        });
+    }
+};
